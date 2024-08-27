363ca6f8df97fabc48b65d7ecdf88a5cf000a4ba
Greg Gallant <ggallant2017@gmail.com>
2024-08-26 18:19:34 -0700

    Fixed headers update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,18 +33,6 @@
 		flex-direction: column;
 		min-height: 100vh;
 	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 0;
-		width: 100%;
-		/*max-width: 64rem;*/
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
 	footer {
 		display: flex;
 		flex-direction: column;
