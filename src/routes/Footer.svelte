fda2b8e88956a37973f9c356d321401809c4833a
Greg Gallant <ggallant2017@gmail.com>
2024-08-26 19:56:22 -0700

    Style updates for headers and links

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -6,10 +6,14 @@
         <div class="foot-generic"><img src="{gmedialogo}" /></div>
         <div class="foot_place_1">
             <div>
-                <ul class="mini_footy">
+                <ul class="footer_links">
                     <!-- <li><a target="github" href="http://www.gallantmedia.net">Code</a></li> -->
-                    <li><a target="github" href="http://www.github.com/GregGallant">GitHub</a></li>
+                    <li><a href="/news">Creative Links</a></li>
+                    <li><a href="/about">On Development</a></li>
+                    <li><a href="http://www.github.com/GregGallant">GitHub</a></li>
                     <li><a target="linkedin" href="http://www.linkedin.com/in/greggallant">Linked-In</a></li>
+                    <li><a target="hatecrimeprevention" href="https://www.justice.gov/hatecrimes/spotlight/combating-hate-crimes">Hate Crime Prevention</a></li>
+
                     <!-- <li><a target="software" href="http://www.cronvega.com">Company</a></li> -->
                 </ul>
             </div>
