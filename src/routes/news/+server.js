34ece385d89162ebbb77574cc769fe2f9cdfd9d7
Greg Gallant <ggallant2017@gmail.com>
2025-08-04 17:39:07 -0700

    Update +server.js comments

--- src/routes/news/+server.js
+++ src/routes/news/+server.js
@@ -1,31 +1,19 @@
 import { dev } from '$app/environment';
 import { json, error } from '@sveltejs/kit';
 
-// Used locally via dev server only
-//export const prerender = true;
-
 /** @type {import('./$types').RequestHandler) */
 export const GET = async ({fetch}) => {
 
-    // *** STAGE/PROD ***/
-    //let newsURL = 'https://golang-ggc:8888/webhose';
-    let newsURL = 'https://www.gallantmedia.net:8888/webhose';
-
-    // *** DEV TEST TODO: env vars ***/
-    //let newsURL = 'http://localhost:8888/webhose';
+    // Async fetching Svelte style.  Btw, Staging / Prod urls removed...
 
     const response = await fetch(newsURL);
     let responseJson = '';
-
-    //console.log(response);
     if (response.ok) {
         responseJson = await response.json();
     } else {
         throw error(response.status, response.statusText)
     }
 
-    //const products = await (await import('$lib/dummy-products.json')).default;
-    //return new Response(JSON.stringify(products), {status: 200});
     return json(responseJson, {status: 200});
 }
 
